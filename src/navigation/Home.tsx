4206a8fa643676770d921a9045f6648256bcead3
Daria <dar.zavaliuk@gmail.com>
2023-10-17 18:40:31 +0300

    fix: add loader user with useEffect

--- src/navigation/Home.tsx
+++ src/navigation/Home.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import {StatusBar, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import {useSelector} from 'react-redux';
 import Auth from "./Auth";
 import {Loader} from "../components/Loader/Loader";
+import {Main} from "./Main";
+import {loadUser} from "../redux/actions/loadUser";
+import Store from "../redux/store";
 
 const HomeScreen = () => {
     const {isAuthenticated, loading} = useSelector((state: any) => state.user);
+
+    useEffect(() => {
+        Store.dispatch(loadUser());
+    }, []);
 
     return (
         <>
@@ -23,7 +30,7 @@ const HomeScreen = () => {
                     ) : (
                         <NavigationContainer>
                             {isAuthenticated ? (
-                                <Text>Main</Text>
+                                <Main/>
                             ) : (
                                 <Auth/>
                             )}
