bdb0e56f5b4673852c0484feafed658040acc00a
Daria <dar.zavaliuk@gmail.com>
2023-10-18 23:12:02 +0300

    fix: change to dispatch = useDispatch()

--- src/navigation/Home.tsx
+++ src/navigation/Home.tsx
@@ -1,19 +1,20 @@
 import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Auth from "./Auth";
 import {Main} from "./Main";
-import {loadUser} from "../redux/actions/loadUser";
-import Store from "../redux/store";
+import {loadUser, LoadUserAction} from "../redux/actions/loadUser";
 import {RootState} from "../redux/reducers/rootReducer";
+import {ThunkDispatch} from "@reduxjs/toolkit";
 
 const HomeScreen = () => {
     const selectIsAuthenticated = (state: RootState) => state.user.isAuthenticated;
     const isAuthenticated = useSelector(selectIsAuthenticated);
 
+    const dispatch = useDispatch<ThunkDispatch<RootState, undefined, LoadUserAction>>();
     useEffect(() => {
-        Store.dispatch(loadUser());
+        dispatch(loadUser());
     }, []);
 
     return (
