5e4655c5f907337132853ea343ce0e17a8e7e10c
Daria <dar.zavaliuk@gmail.com>
2023-10-23 01:18:57 +0300

    fix: move types to createAction

--- src/redux/actions/resetPassword.ts
+++ src/redux/actions/resetPassword.ts
@@ -1,16 +1,12 @@
-import { createAction } from "@reduxjs/toolkit";
-import axios, { AxiosError } from "axios";
-import { URI } from "../../URI";
-import { Dispatch } from "redux";
+import {createAction} from "@reduxjs/toolkit";
+import axios, {AxiosError} from "axios";
+import {URI} from "../../URI";
+import {Dispatch} from "redux";
 import * as types from "../actionTypes/actionTypes";
 
-type ResetPasswordRequestPayload = void;
-type ResetPasswordSuccessPayload = { data: any };
-type ResetPasswordFailedPayload = { error: string };
-
-const resetPasswordRequest = createAction<ResetPasswordRequestPayload>(types.RESET_PASSWORD_REQUEST);
-const resetPasswordSuccess = createAction<ResetPasswordSuccessPayload>(types.RESET_PASSWORD_SUCCESS);
-const resetPasswordFailed = createAction<ResetPasswordFailedPayload>(types.RESET_PASSWORD_FAILED);
+const resetPasswordRequest = createAction(types.RESET_PASSWORD_REQUEST);
+const resetPasswordSuccess = createAction<{ data: any }>(types.RESET_PASSWORD_SUCCESS);
+const resetPasswordFailed = createAction<{ error: string }>(types.RESET_PASSWORD_FAILED);
 
 type ResetPasswordAction =
     | ReturnType<typeof resetPasswordRequest>
@@ -21,14 +17,18 @@ export const resetPassword = (email: string, password: string) => async (dispatc
     try {
         dispatch(resetPasswordRequest());
 
-        const config = { headers: { "Content-Type": "application/json" } };
+        const config = {headers: {"Content-Type": "application/json"}};
 
-        const { data } = await axios.post(`${URI}/reset-password`, { email, password }, config);
+        const {data} = await axios.post(`${URI}/reset-password`, {email, password}, config);
 
-        dispatch(resetPasswordSuccess({ data }));
+        dispatch(resetPasswordSuccess({data}));
     } catch (error) {
         dispatch(
-            resetPasswordFailed({ error: (error as AxiosError<{ message: string }>)?.response?.data?.message || "Unexpected error" })
+            resetPasswordFailed({
+                error: (error as AxiosError<{
+                    message: string
+                }>)?.response?.data?.message || "Unexpected error"
+            })
         );
     }
 };
