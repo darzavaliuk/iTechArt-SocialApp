79acca69c9b48376c7c335c0e89c89258fa3bab1
Daria <dar.zavaliuk@gmail.com>
2023-10-23 01:18:21 +0300

    fix: createAction for error reset

--- src/redux/actions/resetError.ts
+++ src/redux/actions/resetError.ts
@@ -1,14 +1,4 @@
-import { Dispatch } from "redux";
 import { createAction } from "@reduxjs/toolkit";
 import * as types from "../actionTypes/actionTypes";
 
-type ResetErrorRequestPayload = void;
-
-const resetErrorRequest = createAction<ResetErrorRequestPayload>(types.RESET_ERROR);
-
-type ResetErrorAction =
-    | ReturnType<typeof resetErrorRequest>
-
-export const resetError = () => async (dispatch: Dispatch<ResetErrorAction>) => {
-    dispatch(resetErrorRequest());
-};
+export const resetErrorRequest = createAction(types.RESET_ERROR);
