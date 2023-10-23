10af6328db4beb326e00193ab7e170803a2fa785
Daria <dar.zavaliuk@gmail.com>
2023-10-23 14:40:00 +0300

    fix: delete promise

--- src/redux/actions/loadUser.ts
+++ src/redux/actions/loadUser.ts
@@ -17,50 +17,27 @@ export type LoadUserAction =
     | ReturnType<typeof loadUserSuccess>
     | ReturnType<typeof loadUserFailed>;
 
-function getTokenRequest(dispatch: Dispatch<LoadUserAction>) {
-    return new Promise(async (resolve, reject) => {
-        setTimeout(async () => {
-            try {
-                console.log("token")
-                const token = await getToken();
-                resolve(token)
-            } catch (error) {
-                dispatch(loadUserFailed((error as AxiosError<{
-                    message: string
-                }>)?.response?.data?.message || "Unexpected error"));
-                reject(error)
-            }
-        }, 0)
-    })
+async function getTokenRequest() {
+    return await getToken();
 }
 
 async function getDataRequest(token: string) {
-    return new Promise(async (resolve, reject) => {
-        setTimeout(async () => {
-            try {
-                console.log(token)
-                const {data} = await axios.get(`${URI}/me`, {
-                    headers: {Authorization: `Bearer ${token}`},
-                });
-                console.log(data)
-                resolve(data);
-            } catch (error) {
-                reject(error)
-            }
-        }, 0)
-    })
+    const {data} = await axios.get(`${URI}/me`, {
+        headers: {Authorization: `Bearer ${token}`},
+    });
+    return data
 }
 
-async function getAllRequests(dispatch: Dispatch<LoadUserAction>) {
-    const token = await getTokenRequest(dispatch) as string;
-    const data = await getDataRequest(token!) as any
+async function getAllRequests() {
+    const token = await getTokenRequest();
+    const data = await getDataRequest(token!)
     return {token, data};
 }
 
 export const loadUser = () => async (dispatch: Dispatch<LoadUserAction>) => {
     try {
         dispatch(loadUserRequest());
-        const {token, data} = await getAllRequests(dispatch)
+        const {token, data} = await getAllRequests()
         dispatch(loadUserSuccess({user: data.user, token}));
     } catch (error: unknown) {
         dispatch(loadUserFailed((error as AxiosError<{
