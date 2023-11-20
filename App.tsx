52e02940da19c14956dd23d77cd77ad3f1c64452
Daria <dar.zavaliuk@gmail.com>
2023-11-20 05:57:49 +0300

    fix: refactor App for custom toast

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {AppStack} from "./src/components/AppStack";
 import {Provider} from 'react-redux';
 import Store from "./src/redux/store";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
+import Toast from "./src/components/Toast/Toast";
+import ToastContext from './src/context/toasterContext';
 
 function App(): JSX.Element {
+    const toastRef = useRef()
+    const showToast = (type, text, duration) => {
+        toastRef?.current?.show({ type, text, duration }) ;
+    };
+
     return (
         <Provider store={Store}>
             <GestureHandlerRootView style={{flex: 1}}>
-                <AppStack/>
+                <ToastContext.Provider value={{ showToast }}>
+                    <Toast ref={toastRef}/>
+                    <AppStack/>
+                </ToastContext.Provider>
             </GestureHandlerRootView>
         </Provider>
     );
