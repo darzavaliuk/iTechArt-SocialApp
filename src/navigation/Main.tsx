4a9ab4821443effdd800bddaa1d8fee2b63e1927
Daria <dar.zavaliuk@gmail.com>
2023-11-29 01:33:43 +0300

    fix: add types

--- src/navigation/Main.tsx
+++ src/navigation/Main.tsx
@@ -10,26 +10,30 @@ import FollowerCard from "../components/FollowerCard/FollowerCard";
 import {TargetsScreen} from "../screens/TargetsScreen/TargetsScreen";
 import {UserProfileScreen} from "../screens/UserProfileScreen/UserProfileScreen";
 import PostScreen from "../screens/CreatePostScreen/CreatePostScreen";
+import {createStackNavigator} from "@react-navigation/stack";
+import {MainStackParamList} from "./MainStack";
+
+const RootStack = createStackNavigator<MainStackParamList>();
 
 export const Main = () => {
     const Stack = createNativeStackNavigator();
     return (
-        <Stack.Navigator
+        <RootStack.Navigator
             screenOptions={{
                 headerShown: false,
             }}>
-            <Stack.Screen name="HomeScreen" component={Tabs}/>
-            <Stack.Screen name="TargetScreen" component={CreateTargetScreen}/>
-            <Stack.Screen name="ConcreteTargetScreen" component={ConcreteTargetScreen}/>
-            <Stack.Screen name="UserProfile" component={ProfileScreen} />
-            <Stack.Screen name="CreateReplies" component={CreateRepliesScreen} />
-            <Stack.Screen name="PostDetails" component={PostDetailsScreen} />
+            <RootStack.Screen name="HomeScreen" component={Tabs}/>
+            <RootStack.Screen name="TargetScreen" component={CreateTargetScreen}/>
+            <RootStack.Screen name="ConcreteTargetScreen" component={ConcreteTargetScreen}/>
+            <RootStack.Screen name="UserProfile" component={ProfileScreen} />
+            <RootStack.Screen name="CreateReplies" component={CreateRepliesScreen} />
+            <RootStack.Screen name="PostDetails" component={PostDetailsScreen} />
             {/*<Stack.Screen name="PostLikeCard" component={PostLikeCard} />*/}
-            <Stack.Screen name="FollowerCard" component={FollowerCard} />
-            <Stack.Screen name="EditProfile" component={EditProfile} />
-            <Stack.Screen name="TargetsScreen" component={TargetsScreen} />
-            <Stack.Screen name="PostScreen" component={PostScreen} />
-            <Stack.Screen name="UserProfileScreen" component={UserProfileScreen} />
-        </Stack.Navigator>
+            <RootStack.Screen name="FollowerCard" component={FollowerCard} />
+            <RootStack.Screen name="EditProfile" component={EditProfile} />
+            <RootStack.Screen name="TargetsScreen" component={TargetsScreen} />
+            <RootStack.Screen name="PostScreen" component={PostScreen} />
+            <RootStack.Screen name="UserProfileScreen" component={UserProfileScreen} />
+        </RootStack.Navigator>
     );
 };
