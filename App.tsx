85c4a932f4947623bbb610f610dd7237cac3797a
Daria <dar.zavaliuk@gmail.com>
2023-11-29 01:29:56 +0300

    fix: update toast provider

--- App.tsx
+++ App.tsx
@@ -1,24 +1,17 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import {AppStack} from "./src/components/AppStack";
 import {Provider} from 'react-redux';
 import Store from "./src/redux/store";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
-import Toast from "./src/components/Toast/Toast";
-import ToastContext from './src/context/toasterContext';
+import {ToastContextProvider} from "./src/context/provider/Provider";
 
 function App(): JSX.Element {
-    const toastRef = useRef()
-    const showToast = (type, text, duration) => {
-        toastRef?.current?.show({ type, text, duration }) ;
-    };
-
     return (
         <Provider store={Store}>
             <GestureHandlerRootView style={{flex: 1}}>
-                <ToastContext.Provider value={{ showToast }}>
-                    <Toast ref={toastRef}/>
+                <ToastContextProvider>
                     <AppStack/>
-                </ToastContext.Provider>
+                </ToastContextProvider>
             </GestureHandlerRootView>
         </Provider>
     );
