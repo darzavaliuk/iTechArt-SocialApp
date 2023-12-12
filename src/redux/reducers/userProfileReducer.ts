fb4c99462630a836a683a55e8852a598c90dfdd3
Daria <dar.zavaliuk@gmail.com>
2023-12-12 10:24:52 +0300

    fix: refactor to toolkit builder

--- src/redux/reducers/userProfileReducer.ts
+++ src/redux/reducers/userProfileReducer.ts
@@ -1,31 +1,32 @@
-const initialState = {
+import {createReducer, PayloadAction} from "@reduxjs/toolkit";
+import {User} from "./User";
+import {profileError, profileRequest, profileSuccess} from "../actions/createAction";
+
+interface ProfileState {
+    loading: boolean;
+    userProfile: User | null;
+    error: string | null;
+}
+
+const initialState: ProfileState = {
     loading: false,
     userProfile: null,
     error: null
 };
 
-export const profileReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'PROFILE_REQUEST':
-            return {
-                ...state,
-                loading: true,
-                error: null
-            };
-        case 'PROFILE_SUCCESS':
-            return {
-                ...state,
-                loading: false,
-                userProfile: action.payload,
-                error: null
-            };
-        case 'PROFILE_ERROR':
-            return {
-                ...state,
-                loading: false,
-                error: action.payload
-            };
-        default:
-            return state;
-    }
-};
+export const profileReducer = createReducer(initialState, (builder) => {
+    builder
+        .addCase(profileRequest, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(profileSuccess, (state, action: PayloadAction<User>) => {
+            state.loading = false;
+            state.userProfile = action.payload;
+            state.error = null;
+        })
+        .addCase(profileError, (state, action: PayloadAction<string>) => {
+            state.loading = false;
+            state.error = action.payload;
+        });
+});
