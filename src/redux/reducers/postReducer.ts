65bbda126dac749cece88cecaa26b882ace589e2
Daria <dar.zavaliuk@gmail.com>
2023-12-12 10:24:03 +0300

    fix: refactor to toolkit builder

--- src/redux/reducers/postReducer.ts
+++ src/redux/reducers/postReducer.ts
@@ -1,62 +1,73 @@
 import {createReducer} from '@reduxjs/toolkit';
 import {Post, Reply} from "./User";
 
-const intialState = {
+interface PostState {
+    isLoading: boolean;
+    isSuccess: boolean;
+    error: string | null;
+    post: Post | null;
+    posts: Post[];
+    postsUser: Post[];
+    repliesUser: Post[];
+}
+
+const initialState: PostState = {
     posts: [] as Post[],
     post: {} as Post,
     error: null,
     isSuccess: false,
     isLoading: true,
     postsUser: [] as Post[],
-    repliesUser: [] as Reply[]
+    repliesUser: [] as Post[]
 };
 
-export const postReducer = createReducer(intialState, {
-    CREATE_POST_SUCCESS: (state, action) => {
-        state.isLoading = false;
-        state.post = action.payload;
-        state.isSuccess = true;
-    },
-    CREATE_POST_FAILED: (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-    },
-    CREATE_POST_REQUEST: state => {
-        state.isLoading = true;
-    },
-    GET_POSTS_SUCCESS: (state, action) => {
-        console.log("dataPosts <<<", action.payload.dataPosts)
-        state.isLoading = false;
-        state.posts = action.payload.dataPosts;
-    },
-    GET_POSTS_FAILED: (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-    },
-    clearErrors: state => {
-        state.error = null;
-    },
-    GET_POST_REQUEST: state => {
-        state.isLoading = true;
-    },
-    GET_POST_SUCCESS: (state, action) => {
-        state.isLoading = false;
-        // console.log(action.payload)
-        state.postsUser = action.payload.dataPosts;
-    },
-    GET_POST_FAILED: (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-    },
-    GET_REPLIES_REQUEST: state => {
-        state.isLoading = true;
-    },
-    GET_REPLIES_SUCCESS: (state, action) => {
-        state.isLoading = false;
-        state.repliesUser = action.payload.dataPosts;
-    },
-    GET_REPLIES_FAILED: (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-    },
-});
+export const postReducer = createReducer(initialState, (builder) => {
+    builder
+        .addCase('CREATE_POST_SUCCESS', (state, action) => {
+            state.isLoading = false
+            state.post = action.payload
+            state.isSuccess = true
+        })
+        .addCase('CREATE_POST_FAILED', (state, action) => {
+            state.isLoading = false
+            state.error = action.payload
+        })
+        .addCase('CREATE_POST_REQUEST', (state) => {
+            state.isLoading = true
+        })
+        .addCase('GET_POSTS_SUCCESS', (state, action) => {
+            console.log('dataPosts <<<', action.payload.dataPosts)
+            state.isLoading = false
+            state.posts = action.payload.dataPosts
+        })
+        .addCase('GET_POSTS_FAILED', (state, action) => {
+            state.isLoading = false
+            state.error = action.payload
+        })
+        .addCase('clearErrors', (state) => {
+            state.error = null
+        })
+        .addCase('GET_POST_REQUEST', (state) => {
+            state.isLoading = true
+        })
+        .addCase('GET_POST_SUCCESS', (state, action) => {
+            state.isLoading = false
+            // console.log(action.payload)
+            state.postsUser = action.payload.dataPosts
+        })
+        .addCase('GET_POST_FAILED', (state, action) => {
+            state.isLoading = false
+            state.error = action.payload
+        })
+        .addCase('GET_REPLIES_REQUEST', (state) => {
+            state.isLoading = true
+        })
+        .addCase('GET_REPLIES_SUCCESS', (state, action) => {
+            state.isLoading = false
+            state.repliesUser = action.payload.dataPosts
+        })
+        .addCase('GET_REPLIES_FAILED', (state, action) => {
+            state.isLoading = false
+            state.error = action.payload
+        })
+})
