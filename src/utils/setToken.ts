b8429e437378d2d25ba404765b4f69df58f58940
Daria <dar.zavaliuk@gmail.com>
2023-12-12 10:20:45 +0300

    fix: add refresh token

--- src/utils/setToken.ts
+++ src/utils/setToken.ts
@@ -2,7 +2,15 @@ import * as Keychain from 'react-native-keychain';
 
 export const setToken = async (token: string) => {
     try {
-        await Keychain.setGenericPassword('token', token);
+        await Keychain.setInternetCredentials('token', "token", token)
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const setRefreshToken = async (token: string) => {
+    try {
+        await Keychain.setInternetCredentials('refreshToken', "refreshToken", token);
     } catch (error) {
         console.log(error);
     }
